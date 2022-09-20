006bbf4b5665ad0215f6edd40b96c80ad45f6143
JM <georgemares14@yahoo.com>
2022-09-20 13:20:03 -0700

    Created needed models

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    username: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    reactions:[reactionSchema],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
